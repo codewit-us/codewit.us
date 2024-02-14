9a73dfd594f630be13c3f1028582fdbe6ec34ead
KavetiRohith <rohitkaveti@gmail.com>
2024-02-14 12:35:51 -0800

    add routes,controllers and models for demos and exercises

---
+++ codewit/api/src/controllers/demo.ts
@@ -0,0 +1,40 @@
+import { Demo, Exercise } from '../models';
+
+async function getAllDemos(): Promise<Demo[]> {
+  return await Demo.findAll({ include: Exercise });
+}
+
+async function getDemoById(uid: number): Promise<Demo | null> {
+  return await Demo.findByPk(uid, { include: Exercise });
+}
+
+async function createDemo(title: string, youtube_id: string): Promise<Demo> {
+  return await Demo.create({ title, youtube_id });
+}
+
+async function updateDemo(
+  uid: number,
+  title?: string,
+  youtube_id?: string
+): Promise<Demo | null> {
+  const demo = await Demo.findByPk(uid);
+  if (demo) {
+    if (title) demo.title = title;
+    if (youtube_id) demo.youtube_id = youtube_id;
+
+    await demo.save();
+  }
+
+  return demo;
+}
+
+async function deleteDemo(uid: number): Promise<Demo | null> {
+  const demo = await Demo.findByPk(uid);
+  if (demo) {
+    await demo.destroy();
+  }
+
+  return demo;
+}
+
+export { getAllDemos, getDemoById, createDemo, updateDemo, deleteDemo };
---
+++ codewit/api/src/models/index.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize';
+import { Demo } from './demo';
+import { Exercise } from './exercise';
+
+const sequelize = new Sequelize({
+  database: 'codewitus_db',
+  dialect: 'postgres',
+  username: 'rohith',
+  password: '12345',
+  port: 5432,
+});
+
+[Demo, Exercise].forEach((model) => model.initialize(sequelize));
+
+Demo.hasMany(Exercise, { foreignKey: 'demo_uid' });
+Exercise.belongsTo(Demo, { foreignKey: 'demo_uid' });
+
+export { Demo, Exercise, sequelize };
