7a94597a8dbcac7c73d09d169e9efc5facbcfa09
numan-7 <numankhan165@gmail.com>
2024-03-13 13:42:23 -0700

    Setup Language & Tag Libraries

---
+++ codewit/lib/shared/tag/src/lib/tag.ts
@@ -0,0 +1,14 @@
+class tag {
+  name: string = '';
+
+  // constructor(name: string) {
+  //   this.name = name;
+  // }
+
+  // TODO: Implement the add method, make sure there are no duplicates
+  async add(tagName: string): Promise<void> {
+   
+  }
+}
+
+export default tag;
