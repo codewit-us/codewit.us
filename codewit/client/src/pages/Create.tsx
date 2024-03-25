ef3c98b19f7cec050b614f2d42bf068a23d0c686
Numan Khan <101899800+numan-7@users.noreply.github.com>
2024-03-24 19:27:57 -0700

    Created Resource Form Skeleton

--- codewit/client/src/pages/Create.tsx
+++ codewit/client/src/pages/Create.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
-import { VideoCameraIcon, BookOpenIcon, LinkIcon} from '@heroicons/react/24/outline'
+import { VideoCameraIcon, BookOpenIcon, LinkIcon, QueueListIcon, CommandLineIcon} from '@heroicons/react/24/outline'
 
 const Create = (): JSX.Element => {
 
@@ -7,8 +7,16 @@ const Create = (): JSX.Element => {
     <div className="md:flex w-full h-container-full">
       <div className="w-full md:w-40 md:h-full bg-background-500 border-solid border-r-1 border-white md:border-gray-800 md:border-r">
         <div className="flex font-medium text- flex-col p-4">
-          <Link to="/create" className="flex items-center gap-2 text-gray-300 p-2 hover:bg-accent-900 rounded-md">
+          <Link to="/create/module" className="flex items-center gap-2 text-gray-300 p-2 hover:bg-accent-900 rounded-md">
+            <QueueListIcon className="w-5 h-5" />
+            Module
+          </Link>
+          <Link to="/create/course" className="flex items-center gap-2 text-gray-300 p-2 hover:bg-accent-900 rounded-md">
             <VideoCameraIcon className="w-5 h-5" />
+            Course
+          </Link>
+          <Link to="/create/demo" className="flex items-center gap-2 text-gray-300 p-2 hover:bg-accent-900 rounded-md">
+            <CommandLineIcon className="w-5 h-5" />
             Demo
           </Link>
           <Link to="/create/exercise" className="flex items-center gap-2 text-gray-300 p-2 hover:bg-accent-900 rounded-md">
