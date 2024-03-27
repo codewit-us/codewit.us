1207b1dab3dc02da8159c47bbd26b819a41b395c
Numan Khan <101899800+numan-7@users.noreply.github.com>
2024-03-27 16:40:23 -0700

    Added Editing/Deleting to Module, Resource, and Course Forms

--- codewit/client/src/pages/ResourceForm.tsx
+++ codewit/client/src/pages/ResourceForm.tsx
@@ -1,15 +1,32 @@
-import {useState} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import SubmitBtn from '../components/form/SubmitButton';
 import InputLabel from '../components/form/InputLabel';
 import TextInput from '../components/form/TextInput';
+import ExistingTable from '../components/form/ExistingTable';
+
+interface Resource {
+  url: string;
+  title: string;
+  source: string;
+  likes: number;
+  uid?: number
+}
+
 
 const ResourceForm = (): JSX.Element => {
-  const [resource, setResource] = useState({
+  const [resource, setResource] = useState<Resource>({
     url: '',
     title: '',
     source: '',
-    likes: 1
+    likes: 1,
   });
+  const [existingResources, setExistingResources] = useState<Resource[]>([]); 
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    const storedResources = JSON.parse(localStorage.getItem('resources') || '[]');
+    setExistingResources(storedResources);
+  }, [])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -19,28 +36,48 @@ const ResourceForm = (): JSX.Element => {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleEdit = useCallback((uid: number) => {
+    const editResource = existingResources.find((res) => res.uid === uid);
+    if (editResource) {
+      setIsEditing(true);
+      setResource(editResource);
+    }
+  }, [existingResources]); 
+  
+  const handleDelete = useCallback((uid: number) => {
+    const updatedResources = existingResources.filter((res) => res.uid !== uid);
+    setExistingResources(updatedResources);
+  }, [existingResources]);
+  
+  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const newResource = { ...resource, id: Date.now() };
-    const existingResources = JSON.parse(localStorage.getItem('resources') || '[]');
-    existingResources.push(newResource);
-    localStorage.setItem('resources', JSON.stringify(existingResources));
-    console.log('submitted', newResource);
-    setResource({ url: '', title: '', source: '', likes: 1 });
-  };
-
+    if (isEditing) {
+      const updatedResources = existingResources.map((res) => res.uid === resource.uid ? resource : res);
+      localStorage.setItem('resources', JSON.stringify(updatedResources));
+      setExistingResources(updatedResources);
+      setResource({ url: '', title: '', source: '', likes: 1, uid: -1 });
+      setIsEditing(false);
+    } else {
+      const newResource = { ...resource, uid: Date.now() };
+      const updatedResources = [...existingResources, newResource];
+      localStorage.setItem('resources', JSON.stringify(updatedResources));
+      setExistingResources(updatedResources);
+      console.log('submitted', newResource);
+      setResource({ url: '', title: '', source: '', likes: 1, uid: -1 });
+    }
+  }, [existingResources, resource, isEditing]);
 
   return (
-    <div className="flex justify-center p-4 items-start h-full bg-zinc-900 overflow-auto">
+    <div className="flex gap-2 justify-center p-4 items-start h-full bg-zinc-900 overflow-auto">
       <form onSubmit={handleSubmit} className="bg-gray-800 rounded-md bg-opacity-50 w-full max-w-4xl h-full p-6 space-y-6">
         <h2 className="text-xl font-semibold text-white">Create Resource</h2>
         <div>
-          <InputLabel htmlFor="url">URL</InputLabel>
-          <TextInput id="url" value={resource.url} name="url" placeholder="Enter URL" onChange={handleChange} required />
-        </div>
-        <div>
           <InputLabel htmlFor="title">Title</InputLabel>
           <TextInput id="title" value={resource.title} name="title" placeholder="Enter Title" onChange={handleChange} required />
+        </div>
+        <div>
+          <InputLabel htmlFor="url">URL</InputLabel>
+          <TextInput id="url" value={resource.url} name="url" placeholder="Enter URL" onChange={handleChange} required />
         </div>
         <div>
           <InputLabel htmlFor="source">Source</InputLabel>
@@ -48,9 +85,15 @@ const ResourceForm = (): JSX.Element => {
         </div>
         <SubmitBtn 
           disabled={resource.url === '' || resource.title === '' || resource.source === ''}
-          text={'Create'} 
+          text={isEditing ? 'Confirm Edit' : 'Create'} 
         />
       </form>
+      <ExistingTable
+        items={existingResources}
+        name="Resources"
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
     </div>
   );
 };
