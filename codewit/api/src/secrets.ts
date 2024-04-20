4a76d88e656dcbcb4fce0c3fea05dea86e1e4787
KavetiRohith <rohitkaveti@gmail.com>
2024-04-20 16:21:24 -0700

    complete basic user sign and sign up flows and add some basic .ejs views to test the same

--- codewit/api/src/secrets.ts
+++ codewit/api/src/secrets.ts
@@ -3,6 +3,7 @@ const PORT = process.env.API_PORT ? Number(process.env.API_PORT) : 3000;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
+const COOKIE_KEY = process.env.COOKIE_KEY as string;
 
 export {
   HOST,
@@ -10,4 +11,5 @@ export {
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URL,
+  COOKIE_KEY,
 };
