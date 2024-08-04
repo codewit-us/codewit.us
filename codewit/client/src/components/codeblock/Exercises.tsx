3424f5b30cd91dad9c1bccad0aeca1f556a8bce6
Numan Khan <101899800+numan-7@users.noreply.github.com>
2024-08-03 22:28:14 -0700

    link attempt submission to backend

--- codewit/client/src/components/codeblock/Exercises.tsx
+++ codewit/client/src/components/codeblock/Exercises.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import MDEditor from '@uiw/react-markdown-editor';
 import { Exercise } from '@codewit/interfaces';
+import { useEffect } from 'react';
 
 interface ExercisesProps {
   exercises: Exercise[];
+  idx: number;
 }
 
-const Exercises = ({ exercises }: ExercisesProps): JSX.Element => {
+const Exercises = ({ exercises, idx }: ExercisesProps): JSX.Element => {
+
   return (
     <div className="w-full">
-      {exercises.map((exercise, index) => (
-        <div key={index} className="mb-2">
-          <h3 className="font-semibold text-lg bg-accent-700 text-white p-2 rounded-t">
-            Exercise {index + 1}
-          </h3>
-          <MDEditor.Markdown source={exercise.prompt} className="bg-none p-2"/>
-        </div>
-      ))}
+      <div key={idx} className="mb-2">
+        <h3 className="font-semibold text-lg bg-accent-700 text-white p-2 rounded-t">
+          Exercise {idx + 1}
+        </h3>
+        <MDEditor.Markdown source={exercises[idx].prompt} className="bg-none p-2"/>
+      </div>
     </div>
   );
 };
