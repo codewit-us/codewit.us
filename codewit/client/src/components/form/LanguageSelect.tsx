9e831292f6171786b522c7b47b54133ab0815ffe
Tony-Ton3 <tonyfier11@gmail.com>
2024-11-07 14:22:44 -0800

    refactor form ui

--- codewit/client/src/components/form/LanguageSelect.tsx
+++ codewit/client/src/components/form/LanguageSelect.tsx
@@ -1,35 +1,3 @@
-// interface LanguageSelectProps {
-//   handleChange : (e: React.ChangeEvent<HTMLSelectElement>) => void;
-//   initialLanguage: string;
-// }
-
-// const LanguageSelect = ({handleChange, initialLanguage}: LanguageSelectProps): JSX.Element => {
-//   return (
-//     <div className="w-full">
-//       <label 
-//         htmlFor="language" 
-//         className="block text-sm font-medium text-gray-200 mb-2"
-//       >
-//         Language
-//       </label>
-//       <select 
-//         id="language" 
-//         name="language" 
-//         onChange={handleChange}
-//         className="w-full text-sm bg-gray-700 text-white rounded-lg border border-gray-600 focus:ring-2 focus:ring-blue-500 focus:border-transparent py-2 px-3"
-//         required
-//         value={initialLanguage.length > 0 ? initialLanguage : "cpp"}
-//       >
-//         <option value="cpp">C++</option>
-//         <option value="java">Java</option>
-//         <option value="Python">Python</option>
-//       </select>
-//     </div>
-//   );
-// }
-
-// export default LanguageSelect;
-
 import { SelectStyles } from '../../utils/styles.js';
 import Select from 'react-select';
 
