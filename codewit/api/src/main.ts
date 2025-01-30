96c3b9ab110f207cc4eeee4ccbe63156c0a5e20c
Rohith <rohitkaveti@gmail.com>
2025-01-30 14:19:36 -0800

    Codeeval integration (#50)
    * initial changes
    
    * serve frontend via nginx too
    
    * set REDIS_HOST and REDIS_PORT for codeeval container
    
    * update ROADMAP.md

--- codewit/api/src/main.ts
+++ codewit/api/src/main.ts
@@ -10,14 +10,29 @@ import userRouter from './routes/user';
 import attemptRouter from './routes/attempt';
 import passport from 'passport';
 import session from 'express-session';
-import { COOKIE_KEY, HOST, PORT } from './secrets';
+import { COOKIE_KEY, HOST, PORT, REDIS_HOST, REDIS_PORT } from './secrets';
 import './auth/passport';
 import { checkAuth } from './middleware/auth';
+import { RedisStore } from "connect-redis"
+import { createClient } from "redis"
 
 const app = express();
 
+let redisClient = createClient(
+  {
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  }
+)
+redisClient.connect().catch(console.error)
+
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "codewit:",
+})
+
 app.use(
   session({
+    store: redisStore,
     secret: COOKIE_KEY,
     resave: false,
     saveUninitialized: false,
