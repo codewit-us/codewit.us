8428fe4ebd4bc25078d20bc97a3a7b7a16b1b5c7
Tony-Ton3 <tongyuzhu1026@gmail.com>
2025-02-04 19:07:00 -0800

    minor ui changes

--- codewit/client/src/components/videoui/HelpfulLinks.tsx
+++ codewit/client/src/components/videoui/HelpfulLinks.tsx
@@ -1,11 +1,9 @@
 // codewit/client/src/components/videoui/HelpfulLinks.tsx
 import { LinkIcon } from "@heroicons/react/24/solid";
 
-const HelpfulLinks = ({ setHelpfulLinksOpen }: { 
-setHelpfulLinksOpen: React.Dispatch<React.SetStateAction<boolean>> 
-}): JSX.Element => (
+const HelpfulLinks = (): JSX.Element => (
   <details className="p-2 font-bold rounded-lg w-full text-white flex flex-col flow-hidden">
-    <summary className = "cursor-pointer" onClick={() => setHelpfulLinksOpen(prev => !prev)}>Helpful Links</summary>
+    <summary className = "cursor-pointer">Helpful Links</summary>
       <a href="#" className = "inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-600 hover:cursor-pointer">
         <LinkIcon fill="#9F2B68" className="w-6 h-6" />
         Java Array Documentaiton
--- codewit/client/src/components/videoui/RelatedDemos.tsx
+++ codewit/client/src/components/videoui/RelatedDemos.tsx
@@ -2,12 +2,9 @@
 import { VideoCameraIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
-// const RelatedDemos = ({ setRelatedDemosOpen }: { setRelatedDemosOpen: (value: boolean) => void }): JSX.Element => (
-const RelatedDemos = ({ setRelatedDemosOpen }: { 
-  setRelatedDemosOpen: React.Dispatch<React.SetStateAction<boolean>> 
-}): JSX.Element => (
+const RelatedDemos = (): JSX.Element => (
   <details className="p-2 font-bold rounded-lg w-full text-white flex flex-col  overflow-hidden">
-    <summary className="cursor-pointer" onClick={() => setRelatedDemosOpen(prev => !prev)}>Related Demos</summary>
+    <summary className="cursor-pointer">Related Demos</summary>
     <a href="#" className="inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-700 hover:cursor-pointer">
       <div className="bg-background-400 p-3 rounded-sm">
         <VideoCameraIcon className="w-4 h-4" />
