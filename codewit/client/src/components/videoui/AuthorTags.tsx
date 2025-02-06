f0b57971335f0d02f7ad1e35aaf58cc85b3c579e
numan-7 <101899800+numan-7@users.noreply.github.com>
2025-02-06 10:44:28 -0800

    fix: accordion flex issue

--- codewit/client/src/components/videoui/AuthorTags.tsx
+++ codewit/client/src/components/videoui/AuthorTags.tsx
@@ -6,22 +6,25 @@ interface AuthorTagsProps {
   tags: Tag[] | string[] | undefined;
 }
 
-const AuthorTags = ({tags}: AuthorTagsProps): JSX.Element => (
-  <div className="flex items-center space-x-2 text-white">
-    <span className = "inline-flex justify-center items-center gap-1 text-lg font-medium"> 
-      by 
-      <UserCircleIcon className="w-7 h-7" />
-      Jessica
-    </span>
-    { tags 
-      ?
-        (tags as Tag[]).map((tag: Tag, index: number) => (
-          <span key={index} className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">{tag.name}</span>
-        ))
-      :
-        ''
-    }
-  </div>
-);
+const AuthorTags = ({tags}: AuthorTagsProps): JSX.Element => {
+console.log(tags)
+  return (
+    <div className="flex items-center space-x-2 text-white">
+      <span className = "inline-flex justify-center items-center gap-1 text-lg font-medium"> 
+        by 
+        <UserCircleIcon className="w-7 h-7" />
+        Jessica
+      </span>
+      { tags 
+        ?
+          (tags as Tag[]).map((tag: Tag, index: number) => (
+            <span key={index} className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">{tag.name}</span>
+          ))
+        :
+          ''
+      }
+    </div>
+  )
+};
 
 export default AuthorTags;
--- codewit/client/src/components/videoui/RelatedDemos.tsx
+++ codewit/client/src/components/videoui/RelatedDemos.tsx
@@ -3,20 +3,22 @@ import { VideoCameraIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 const RelatedDemos = (): JSX.Element => (
-  <details className="p-2 font-bold rounded-lg w-full text-white flex flex-col  overflow-hidden">
+  <details className="p-2 font-bold rounded-lg w-full text-white flex-col overflow-hidden">
     <summary className="cursor-pointer">Related Demos</summary>
-    <a href="#" className="inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-700 hover:cursor-pointer">
-      <div className="bg-background-400 p-3 rounded-sm">
-        <VideoCameraIcon className="w-4 h-4" />
-      </div>
-        Title of Other Video
-    </a>
-    <a href="#" className = "inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-700 hover:cursor-pointer">
-      <div className="bg-background-400 p-3 rounded-sm">
-        <VideoCameraIcon className="w-4 h-4" />
-      </div>
-        Title of Other Video
-    </a>
+    <div className = "flex flex-col">
+      <a href="#" className="inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-700 hover:cursor-pointer">
+        <div className="bg-background-400 p-3 rounded-sm">
+          <VideoCameraIcon className="w-4 h-4" />
+        </div>
+          Title of Other Video
+      </a>
+      <a href="#" className = "inline-flex justify-left items-center pl-2 pt-2 gap-2 hover:text-purple-700 hover:cursor-pointer">
+        <div className="bg-background-400 p-3 rounded-sm">
+          <VideoCameraIcon className="w-4 h-4" />
+        </div>
+          Title of Other Video
+      </a>
+    </div>
   </details>
 );
 
