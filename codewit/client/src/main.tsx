8e88757d93cf45540455fd04c7004b21f76fa3e0
dac098 <me@dac098.me>
2025-07-23 11:31:07 -0700

    Read page refactor #94
    after having a look at the codeblock, I decided to refactor the page to
    also include the changes needed for the codeblock. in another part of
    the repo someone included a package that is the monaco editor but
    wrapped in the necessary react code. I swapped over to using that and
    adjusted what was needed to get the same functionality. also included
    @tanstack/react-query and @tanstack/react-form to help with handling
    forms and requests/responses to the api server. made various changes to
    markup to reduced elements and some of the styling that was previousely
    there.

--- codewit/client/src/main.tsx
+++ codewit/client/src/main.tsx
@@ -2,18 +2,24 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from './app/app';
 
+const query_client = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   // <StrictMode>
+  <QueryClientProvider client={query_client}>
     <BrowserRouter>
       <App />
       <ToastContainer position="bottom-right" autoClose={1000} />
     </BrowserRouter>
+  </QueryClientProvider>
   // </StrictMode>
 );
