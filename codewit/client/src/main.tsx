cff38c1317aa4fd10f3c2101da6fca98ee0fdff6
dac098 <me@dac098.me>
2025-11-28 19:08:02 -0800

    youtube video fallback
    added in a fallback set of inputs if for some reason there is an error
    when attempting to load the available videos from youtube for the
    channel. also added in the `ReactQueryDevtools` to the app to help with
    inspecting data loaded by the react-query client.

--- codewit/client/src/main.tsx
+++ codewit/client/src/main.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from './app/app';
@@ -21,6 +22,7 @@ root.render(
       <App />
       <ToastContainer position="bottom-right" autoClose={1000} />
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false}/>
   </QueryClientProvider>
   // </StrictMode>
 );
