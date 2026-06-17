f8e315a19d41973899368d33be99ccba1a565ae4
DAC <me@dac098.me>
2026-06-17 13:45:02 -0700

    enable asset serving flag and npm fix #118
    - adds in a flag to enable the asset serving for the api server. mainly
      for production as of right now it will require configuration to figure
      out how to make it work.
    - adds some comments in main
    - fixed an `npm ci` issue that still is happening, needs further
      investigation.

--- codewit/api/src/main.ts
+++ codewit/api/src/main.ts
@@ -10,7 +10,7 @@ import userRouter from './routes/user';
 import attemptRouter from './routes/attempt';
 import passport from 'passport';
 import session from 'express-session';
-import { COOKIE_KEY, HOST, PORT } from './secrets';
+import { ENABLE_ASSET_SERVING, COOKIE_KEY, HOST, PORT } from './secrets';
 import './auth/passport';
 import { checkAuth } from './middleware/auth';
 import { catchError, asyncHandle } from "./middleware/catch";
@@ -27,6 +27,8 @@ const assets_dir = join(cwd, "dist/apps/client");
 
 const app = express();
 
+// ensure that a request id and uuid have been assigned to each inbound http
+// request
 app.use(set_request_id);
 app.use(log_request);
 
@@ -52,39 +54,60 @@ app.use(express.json());
 // with this but it should be fixed so that the flow is more consistent
 app.use('/oauth2', authrouter);
 
-// start api configuration -----------------------------------------------------
+if (ENABLE_ASSET_SERVING) {
+  // start api configuration -----------------------------------------------------
 
-const api_router = Router();
+  const api_router = Router();
 
-api_router.use('/oauth2', authrouter);
+  api_router.use('/oauth2', authrouter);
 
-api_router.use('/users', checkAuth, userRouter);
-api_router.use('/demos', checkAuth, demoRouter);
-api_router.use('/exercises', checkAuth, exerciseRouter);
-api_router.use('/modules', checkAuth, moduleRouter);
-api_router.use('/resources', checkAuth, resourceRouter);
-api_router.use('/courses', (req, res, next) => {
-  if (req.path === '/landing' || req.path === '/landing/') {
-    return next();
-  }
+  api_router.use('/users', checkAuth, userRouter);
+  api_router.use('/demos', checkAuth, demoRouter);
+  api_router.use('/exercises', checkAuth, exerciseRouter);
+  api_router.use('/modules', checkAuth, moduleRouter);
+  api_router.use('/resources', checkAuth, resourceRouter);
+  api_router.use('/attempts', checkAuth, attemptRouter);
 
-  return checkAuth(req, res, next);
-}, courseRouter);
-api_router.use('/attempts', checkAuth, attemptRouter);
+  api_router.use('/courses', (req, res, next) => {
+    if (req.path === '/landing' || req.path === '/landing/') {
+      return next();
+    }
 
-app.use("/api", api_router);
+    return checkAuth(req, res, next);
+  }, courseRouter);
 
-// end api configuration -------------------------------------------------------
+  app.use("/api", api_router);
 
-app.get("/*", async (req, res) => {
-  await handler(req, res, {
-    public: assets_dir,
-    rewrites: [
-      { "source": "/**", "destination": "/index.html" }
-    ]
+
+  // handle all requests as if they are trying to retrieve UI assets
+  app.get("/*", async (req, res) => {
+    await handler(req, res, {
+      public: assets_dir,
+      rewrites: [
+        { "source": "/**", "destination": "/index.html" }
+      ]
+    });
   });
-});
 
+  // end api configuration -------------------------------------------------------
+} else {
+  app.use('/users', checkAuth, userRouter);
+  app.use('/demos', checkAuth, demoRouter);
+  app.use('/exercises', checkAuth, exerciseRouter);
+  app.use('/modules', checkAuth, moduleRouter);
+  app.use('/resources', checkAuth, resourceRouter);
+  app.use('/attempts', checkAuth, attemptRouter);
+
+  app.use('/courses', (req, res, next) => {
+    if (req.path === '/landing' || req.path === '/landing/') {
+      return next();
+    }
+
+    return checkAuth(req, res, next);
+  }, courseRouter);
+}
+
+// catch all for dealing with errors
 app.use(catchError);
 
 init()
--- codewit/api/src/secrets.ts
+++ codewit/api/src/secrets.ts
@@ -1,3 +1,11 @@
+function check_if_boolean(value: string): boolean {
+  switch (value.toLowerCase()) {
+    case "true": return true;
+    case "false": return false;
+    default: return false
+  }
+}
+
 const HOST = process.env.API_HOST ?? '0.0.0.0';
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
@@ -5,6 +13,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
 const COOKIE_KEY = process.env.COOKIE_KEY as string;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const ENABLE_ASSET_SERVING = check_if_boolean(process.env.ENABLE_ASSET_SERVING ?? "");
 
 export {
   HOST,
@@ -14,4 +23,5 @@ export {
   GOOGLE_REDIRECT_URL,
   COOKIE_KEY,
   FRONTEND_URL,
+  ENABLE_ASSET_SERVING,
 };
